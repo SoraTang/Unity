fea815a1588cfe46af0da9e0170fe19e8de390cf
TangTvT <2239886711@qq.com>
2023-11-11 02:42:22 +0800

    State

--- My project (1)/Assets/Script/HeroMovement.cs
+++ My project (1)/Assets/Script/HeroMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HeroMovement : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
+    public GameObject idleUI; // 静止状态的UI
+    public GameObject movingUI; // 移动状态的UI
 
     private Rigidbody2D rb;
     public AudioClip footstepSound;
@@ -14,6 +16,7 @@ public class HeroMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         audioSource = GetComponent<AudioSource>();
+        SetUIState(true); // 开始时显示静止状态的UI
     }
 
     void Update()
@@ -25,23 +28,33 @@ public class HeroMovement : MonoBehaviour
         movement.Normalize(); // 防止对角线移动速度过快
 
         rb.velocity = movement * moveSpeed;
+
         if (rb.velocity.magnitude > 0.2f && !audioSource.isPlaying)
         {
             PlayFootstepSound();
+            SetUIState(false); // 切换到移动状态的UI
         }
         if (rb.velocity.magnitude < 0.2f && audioSource.isPlaying)
         {
             StopFootstepSound();
+            SetUIState(true); // 切换到静止状态的UI
         }
     }
+
     void PlayFootstepSound()
     {
         audioSource.clip = footstepSound;
         audioSource.Play();
     }
+
     void StopFootstepSound()
     {
         audioSource.Stop();
     }
-}
 
+    void SetUIState(bool idle)
+    {
+        idleUI.SetActive(idle);
+        movingUI.SetActive(!idle);
+    }
+}
